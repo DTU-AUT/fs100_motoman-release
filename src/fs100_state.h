a1e249b1c531b8884ab323f9feca339b05aa2a7a
AsgerWJ <Teiklyn@gmail.com>
2014-12-16 14:01:48 +0100

    Rebase from 'debian/hydro/fs100_motoman'

--- src/fs100_state.h
+++ src/fs100_state.h
@@ -47,25 +47,28 @@
 #include "motoman_direct_message.h"
 
 
-
+//! FS100 robot state class.
+/*!
+ The FS100 robot state class handles connection with port 50241, which the FS100 uses to send robot joint position data.
+*/
 class Fs100State
 {
 private:
     //private variables and functions
     
-    const char* IP;
-    int sockfd, portno, n, i,run_thread;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    pthread_t recvthreadID;
-    SimpleMsg joint_data; //deserialized data in human readable format
-    bool joint_values_updated;
-    char raw_data[148]; 
+    const char* IP; /*!< Variable storing ip address of target controller. */
+    int sockfd, portno, n, i,run_thread; /*!< Variable for sockect connection. */
+    struct sockaddr_in serv_addr; /*!< Struct for socket connection. */
+    struct hostent *server; /*!< Struct for socket connection. */
+    pthread_t recvthreadID; /*!< Thread ID for data recieving thread.*/
+    SimpleMsg joint_data; /*!< Simple message containing joint data. */
+    bool joint_values_updated; /*!< Has joint data been updated? */
+    char raw_data[148];  /*!< Raw buffer data. */
     
-    pthread_mutex_t mut_lock;
-    pthread_cond_t mut_cond;
-    float current_pos[6];
-    bool pos_updated;
+    pthread_mutex_t mut_lock; /*!< Mutex lock. */
+    pthread_cond_t mut_cond; /*!< Mutex condition. */
+    float current_pos[6]; /*!< Current robot joint position. */
+    bool pos_updated; /*!< Is current robot joint position updated? */
 
     static void *recvDataThread(void *This);
     void recvData();
@@ -82,17 +85,39 @@ private:
 
 public:
     //public variables and functions
+    //! Constructor.
+    /*!
+     Constructor for FS100 state class.
+    */
     Fs100State(const char* ip){
     IP = ip;
     portno = 50241;
     i = 1;
     pthread_mutex_t mut_lock = PTHREAD_MUTEX_INITIALIZER;
     }
+    //! Initiator.
+    /*!
+     Initiates the sockect connection to the FS100 controller, and initializes internal variables.
+    */
     int init();
+    
+    //! Creates connection.
+    /*!
+     Establishes socket connection to the FS100 controller, and starts a thread for recieving data over socket.
+    */
     int makeConnect();
+    
+    //! Closes the class.
+    /*!
+     Closes socket connection and joins all running threads.
+    */
     void pgmClose();
     
-    
+    //! Get updated joint data.
+    /*!
+     A function that returns the next updated joint data. Locks on call until new data is available.
+     \param joints is a pointer to a double[6] type. Used for returning data.
+    */
     bool getJointsUpdated(float* joints);
 };
 
